34e424f9c5c26e97aa2bf47cdd0d27e101602ede
Seb <91453628+Sebras22@users.noreply.github.com>
2022-03-16 16:54:54 +0100

    Add files via upload

---
+++ assets/JS/main.js
@@ -0,0 +1,7 @@
+
+function goRight (container) {
+    document.getElementById(container).scrollLeft += 700;
+};
+function goLeft (container) {
+    document.getElementById(container).scrollLeft -= 700;
+};
