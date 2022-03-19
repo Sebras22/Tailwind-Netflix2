597bc3e43fd374c59d855810fbd49a85bc0f4640
Seb <91453628+Sebras22@users.noreply.github.com>
2022-03-19 22:16:46 +0100

    Delete assets directory

--- assets/JS/main.js
+++
@@ -1,7 +0,0 @@
-
-function goRight (container) {
-    document.getElementById(container).scrollLeft += 700;
-};
-function goLeft (container) {
-    document.getElementById(container).scrollLeft -= 700;
-};
